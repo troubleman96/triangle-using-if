3f1b2fd8b8ebf5d2430ff50053a123fdcb892b91
troubleman96 <itslugenge96@gmail.com>
2024-11-12 13:52:50 +0300

    version uno

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program{
+
+    static void Main(string [] args){
+        int i = 1;
+        while  (i <= 5){
+            int j = 1;
+            while( j <= i){
+                System.Console.Write("*");
+                j++;
+            }
+            System.Console.WriteLine();
+            i++;
+        }
+    }
+}
